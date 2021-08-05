ef5f616dadab2a63d020caa3c287330ae77840df
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-08-05 08:21:26 +0530

    components created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import QuoteBox from './components/QuoteBox'
+
+const App = ()=>{
+    return(
+        <div>
+            <QuoteBox />
+        </div>
+    )
+} 
+
+export default App
---
+++ src/components/QuoteBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import './components/style.css'
+
+const QuoteBox = ()=>{
+    return (
+        <div id="quote-box">
+            From QuoteBox
+        </div>
+    )
+}
+
+export default QuoteBox
