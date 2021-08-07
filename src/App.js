bb167ddc0ece74e2dc1379d86511c71355fb6082
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-08-07 13:40:09 +0530

    almost finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import QuoteBox from './QuoteBox'
 import './App.css'
-const App = ()=>{
+import axios from 'axios'
+const App = () => {
+    const [data,setData] = useState([])
+    const [quote,setQuote] = useState({text:"",author:""})
+    const [bgColor,setBgColor] = useState("")
 
-    const bgcolor = "#77B1A9" 
+    //handling Color
+    const color = [
+        "#77B1A9",
+        "#9B59B6",
+        "#472E32",
+        "#73A857",
+        "#F39C12",
+        "#342224",
+        "#27AE60",
+        "#764ba2",
+        "#2a5298",
+    ]
 
-    return(
-        <div className="body" style={{backgroundColor:bgcolor}}>
-            <QuoteBox quote="The most difficult thing is the decision to act, the rest is merely tenacity." author="Amelia Earhart" color={bgcolor}/>
+    //Handling Quotes From API
+     const sendQuote= ()=>{
+        setQuote(data[Math.floor(Math.random()*data.length)])
+        setBgColor(color[Math.floor(Math.random() * color.length)])
+        console.log(quote);
+    }
+
+    //UseEffect
+    useEffect(()=>{
+        getQuote()
+        sendQuote()
+    },[])
+
+   const getQuote= async ()=>{
+        const response = await axios.get("https://type.fit/api/quotes")
+        setData(response.data)
+    }
+
+    
+
+    return (
+        <div className="body" style={{ backgroundColor: bgColor}}>
+            <QuoteBox quote={quote?.text} author={quote?.author} color={bgColor} getQuote={sendQuote}/>
         </div>
     )
-} 
+}
 
 export default App
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -2,15 +2,17 @@ import React from 'react'
 import './quotebox.css'
 
 const QuoteBox = (props)=>{
+
+
     return (
         <div id="quote-box" style={{color:props.color}}>
         <div className="quote">
-        <blockquote id="text"> <i class="fa fa-quote-left"> </i> {props.quote}</blockquote>
+        <blockquote id="text"> <i className="fa fa-quote-left"> </i> {props.quote?props.quote:"Famous Quote Loading"}</blockquote>
         <p id="author">- {props.author?props.author:"unknown"}</p>
         </div>
         <div className="button-group">
-        <button id="tweet-quote" style={{backgroundColor:props.color}}><i class="fab fa-twitter"></i></button>
-        <button id="new-quote" style={{backgroundColor:props.color}}>New Quote</button>
+        <a id="tweet-quote" style={{backgroundColor:props.color}} rel="noreferrer" target="_blank" href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${props.quote}&via=kritebh`}><i className="fab fa-twitter"></i></a>
+        <button id="new-quote" style={{backgroundColor:props.color}} onClick={props.getQuote}>New Quote</button>
         </div>
         </div>
     )
